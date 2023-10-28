46e08ab6fbfbf0715ed329ab377d200be8b344bc
nobody <109945998+x65534@users.noreply.github.com>
2023-10-28 13:04:40 +1300

    fix command install

--- cmd/swfx/main.go
+++ cmd/swfx/main.go
@@ -1,13 +1,12 @@
 /*
 Copyright © 2023 x65534
-
 */
 package main
 
 import (
 	"github.com/x65534/swfx/cmd/swfx/cmd"
-	_ "github.com/x65534/swfx/cmd/swfx/cmd/ls"
 	_ "github.com/x65534/swfx/cmd/swfx/cmd/extract"
+	_ "github.com/x65534/swfx/cmd/swfx/cmd/ls"
 )
 
 func main() {
